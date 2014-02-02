3a635691111df71aaf7b9c35907ae926a1a09c7e
Jon Higgs <jhiggs@eml.cc>
2014-02-02 17:04:38 +0100

    fix some stuff.

--- lib/btrfs-time-machine/settings.rb
+++ lib/btrfs-time-machine/settings.rb
@@ -1,14 +1,32 @@
 require 'yaml'
 
 module TimeMachine
-  class Config
-    def initialize config
+  class Settings
+    def initialize settings
+      config = settings[:config]
+      sources = settings[:sources]
+
       raise "Config file '#{config}' not found" unless File.exist?(config)
+      raise "Config file '#{sources}' not found" unless File.exist?(sources)
+
       @config = YAML.load_file(config)
+      @sources = YAML.load_file(sources)
+
+      @settings = @config
+      @settings[:sources] = @sources
     end
 
     def to_hash
+      @settings
+    end
+
+    def config
       @config
     end
+
+    def sources
+      @sources
+    end
+
   end
 end
