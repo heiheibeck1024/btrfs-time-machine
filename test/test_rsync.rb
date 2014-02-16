647bb1eccdd2acaa29679dab2a30187f4d2bcbe2
Jon Higgs <jhiggs@eml.cc>
2014-02-16 21:44:47 +0100

    add rsync support for inclusions.

--- test/test_rsync.rb
+++ test/test_rsync.rb
@@ -54,4 +54,30 @@ context "#TimeMachine::Rsync" do
     asserts("options"){topic.options[TEST_DATA]}.includes {"--exclude /blah2"}
     asserts("first command") {topic.commands.first}.equals "/usr/bin/rsync --acls --archive --delete --delete-excluded --human-readable --inplace --no-whole-file --numeric-ids --verbose --xattrs --one-file-system --exclude /blah --exclude /blah2 /tmp/test-tm/mnt/latest/tmp/test-tm/src"
   end
+
+  context "with inclusions" do
+    setup do
+      config = { "backup_mount_point" => MOUNT_POINT }
+      settings = TimeMachine::Settings.new(config)
+      settings.add_source({ "path" => TEST_DATA, "inclusions" => [ "/blah", "/blah2" ]})
+      TimeMachine::Rsync.new(settings)
+    end
+
+    asserts("options"){topic.options[TEST_DATA]}.includes {"--include /blah"}
+    asserts("options"){topic.options[TEST_DATA]}.includes {"--include /blah2"}
+    asserts("first command") {topic.commands.first}.equals "/usr/bin/rsync --acls --archive --delete --delete-excluded --human-readable --inplace --no-whole-file --numeric-ids --verbose --xattrs --one-file-system --include /blah --include /blah2 /tmp/test-tm/mnt/latest/tmp/test-tm/src"
+  end
+
+  context "with inclusions and exclusions" do
+    setup do
+      config = { "backup_mount_point" => MOUNT_POINT }
+      settings = TimeMachine::Settings.new(config)
+      settings.add_source({ "path" => TEST_DATA, "inclusions" => ["/a"], "exclusions" => ["/b"] })
+      TimeMachine::Rsync.new(settings)
+    end
+
+    asserts("options"){topic.options[TEST_DATA]}.includes {"--include /a"}
+    asserts("options"){topic.options[TEST_DATA]}.includes {"--exclude /b"}
+    asserts("include is before exclude") {topic.commands.first}.matches {%r{--include.*--exclude}}
+  end
 end
