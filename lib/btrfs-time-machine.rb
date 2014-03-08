fac0397c32198a49b8ee99f87b4bd41b81054ba4
Jon Higgs <jhiggs@eml.cc>
2014-03-08 11:24:59 +0100

    add command.rb to the app.

--- lib/btrfs-time-machine.rb
+++ lib/btrfs-time-machine.rb
@@ -1,9 +1,9 @@
 $:.unshift File.dirname(__FILE__)
 $:.unshift File.join(File.dirname(__FILE__), '/btrfs-time-machine')
 
+require 'command'
 require 'filesystem'
 require 'logging'
-require 'mixlib/shellout'
 require 'ptools'
 require 'rsync'
 require 'settings'
