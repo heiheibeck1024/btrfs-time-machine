f9d0a1b7e13ce45f754821a8a4a78f7044904524
Jon Higgs <jhiggs@eml.cc>
2014-03-08 14:28:45 +0100

    use denies instead of asserts not.

--- test/test_filesystem.rb
+++ test/test_filesystem.rb
@@ -6,20 +6,21 @@ context "#TimeMachine::FileSystem - btrfs filesystem" do
     TimeMachine::FileSystem.new(BTRFS_FS, MOUNT_POINT)
   }
 
-  asserts("is not mounted") {!topic.mounted?}
+  denies("is not mounted") {topic.mounted?}
   asserts("mount_options is empty") {topic.mount_options.empty?}
   asserts("unmounting unmounted is true") {topic.umount}
   asserts("btrfs_volume? is nil") {topic.btrfs_volume?.nil?}
   asserts("read_only? is nil") {topic.read_only?.nil?}
   asserts("subvolumes is empty") {topic.btrfs_subvolumes.empty?}
+  denies("create subvolume in invalid filesystem") {topic.btrfs_subvolume_create "/tmp/blah"}
 
   context "after mounting" do
     hookup { topic.mount }
     # TODO: test mounting a filesystem on top of a mounted filesystem
     asserts("is now mounted")       {topic.mounted?}
     asserts("is a btrfs volume")    {topic.btrfs_volume?}
-    asserts("is read-write")        {!topic.read_only?}
-    asserts("can not delete non-existant subvolume") {!topic.btrfs_subvolume_delete "x"}
+    denies("is read-write")         {topic.read_only?}
+    asserts("delete non-existant subvolume") {topic.btrfs_subvolume_delete "x"}
     asserts("subvolumes is empty") {topic.btrfs_subvolumes.empty?}
   end
 
@@ -40,25 +41,25 @@ context "#TimeMachine::FileSystem - btrfs filesystem" do
   context "take read-write snapshot" do
     hookup { topic.btrfs_snapshot_create("test", "rwsnap", {:read_only=>false} ) }
     asserts("has a rwsnap directory") {Dir.entries(MOUNT_POINT).include? "rwsnap"}
-    asserts("is not read-only")        {!topic.read_only?("rwsnap")}
+    denies("is not read-only")        {topic.read_only?("rwsnap")}
   end
 
   context "delete subvolume" do
     hookup {topic.btrfs_subvolume_delete "test"}
     asserts("has no test directory") {!Dir.entries(MOUNT_POINT).include? "test"}
-    asserts("has no test subvolume") {!topic.btrfs_subvolumes.include? "test"}
+    denies("has no test subvolume") {topic.btrfs_subvolumes.include? "test"}
   end
 
   context "delete read-only snapshot" do
     hookup {topic.btrfs_snapshot_delete "rosnap"}
-    asserts("has no rosnap directory") {!Dir.entries(MOUNT_POINT).include? "rosnap"}
-    asserts("has no rosnap subvolume") {!topic.btrfs_subvolumes.include? "rosnap"}
+    denies("has no rosnap directory") {Dir.entries(MOUNT_POINT).include? "rosnap"}
+    denies("has no rosnap subvolume") {topic.btrfs_subvolumes.include? "rosnap"}
   end
 
   context "delete read-write snapshot" do
     hookup {topic.btrfs_snapshot_delete "rwsnap"}
-    asserts("has no rwsnap directory") {!Dir.entries(MOUNT_POINT).include? "rwsnap"}
-    asserts("has no rwsnap subvolume") {!topic.btrfs_subvolumes.include? "rwsnap"}
+    denies("has no rwsnap directory") {Dir.entries(MOUNT_POINT).include? "rwsnap"}
+    denies("has no rwsnap subvolume") {topic.btrfs_subvolumes.include? "rwsnap"}
   end
 
   # TODO: get list of snapshots.
@@ -73,7 +74,7 @@ context "#TimeMachine::FileSystem - btrfs filesystem" do
 
   context "after unmounting" do
     hookup { topic.umount }
-    asserts("is now unmounted") {!topic.mounted?}
+    denies("is now unmounted") {topic.mounted?}
   end
 end
 
@@ -82,27 +83,27 @@ context "#TimeMachine::FileSystem - ext4 filesystem" do
     TimeMachine::FileSystem.new(EXT4_FS, MOUNT_POINT)
   }
 
-  asserts("is not mounted") {!topic.mounted?}
+  denies("is not mounted") {topic.mounted?}
   asserts("has unknown btrfs_volume?") {topic.btrfs_volume?.nil?}
   asserts("has unknown read_only?") {topic.read_only?.nil?}
 
   context "after mounting" do
     hookup { topic.mount }
     asserts("is now mounted") {topic.mounted?}
-    asserts("is not a btrfs volume") {!topic.btrfs_volume?}
-    asserts("is read-write") {!topic.read_only?}
+    denies("is not a btrfs volume") {topic.btrfs_volume?}
+    denies("is read-write") {topic.read_only?}
   end
 
   context "after remounting" do
     hookup { topic.remount(["ro"])}
     asserts("is now mounted") {topic.mounted?}
-    asserts("is not a btrfs volume") {!topic.btrfs_volume?}
+    denies("is not a btrfs volume") {topic.btrfs_volume?}
     asserts("is read-only") {topic.read_only?}
   end
 
   context "after unmounting" do
     hookup { topic.umount }
-    asserts("is now unmounted") {!topic.mounted?}
+    denies("is now mounted") {topic.mounted?}
     asserts("has unknown btrfs_volume") {topic.btrfs_volume?.nil?}
     asserts("has unknown read_only?") {topic.read_only?.nil?}
   end
